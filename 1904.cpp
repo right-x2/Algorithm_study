9ec0aa04833286d0ed7e511ecb66cc4104fd36b0
right-x2 <kjw970103@gmail.com>
2020-01-04 21:35:08 +0900

    dp

--- 1904.cpp
+++ 1904.cpp
@@ -2,30 +2,57 @@
 
  
 using namespace std;
+int arr[10000001];
 
-long long arr[1000001];
-void tile (int n)
+int dp(int n,long long cnt)
 {
-    int i = 1;
-    while(i+2<=n)
+    if(n<=1) return 0;
+    if(n%3==0)
     {
-        arr[i+2] = (arr[i]+arr[i+1])%15746;
-        i++;
+        if(arr[n/3]==0||arr[n/3]>cnt+1)
+        {
+            arr[n/3]=cnt+1;
+            dp(n/3,cnt+1);
+        }
+        if(arr[(n-1)/2]==0||arr[(n-1)/2]>cnt+2)
+        {
+            arr[(n-1)/2]=cnt+2;
+            dp((n-1)/2,cnt+2);
+        }
     }
+    else if(n%2==0)
+    {
+        if(arr[n/2]==0||arr[n/2]>cnt+1)
+        {
+            arr[n/2]=cnt+1;
+            dp(n/2,cnt+1);
+        }
+        if(arr[(n-1)/3]==0||arr[(n-3)/2]>cnt+2)
+        {
+            arr[(n-1)/3]=cnt+2;
+            dp((n-1)/3,cnt+2);
+        }
+    }
+    else
+    {
+        if(arr[n-1]==0||arr[n-1]>cnt+1)
+        {
+            arr[n-1]=cnt+1;
+            dp(n-1,cnt+1);
+        }
+    }
+    return 0;
 }
+
+
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
     int n;
-    arr[0] = 0;
-    arr[1] = 1;
-    arr[2] = 2;
-    arr[3] = 3;
-
     cin>>n;
-    tile(n);
-    cout<<arr[n]<<"\n";
+    dp(n,0);
+    cout<<arr[1]<<"\n";
     return 0;
 }  
