4a943fe82bdfbd126310ea4c9e9539f79a6880d4
right-x2 <kjw970103@gmail.com>
2020-01-06 20:03:10 +0900

    basic

---
+++ 2588.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int a,b;
+    cin>>a>>b;
+    cout<<a*(b%10)<<"\n";
+    cout<<a*((b%100)/10)<<"\n";
+    cout<<a*(b/100)<<"\n";
+    cout<<a*b<<"\n";
+ 
+
+    return 0;
+}  
