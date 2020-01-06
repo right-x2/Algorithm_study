47ba9b7270b0fed662f0a8edae82b9b8e36581a3
right-x2 <kjw970103@gmail.com>
2020-01-06 20:45:57 +0900

    basic

---
+++ 10172.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    cout<<"|\\_/|"<<"\n";
+    cout<<"|q p|   /}"<<"\n";
+    cout<<"( 0 )\"\"\"\\"<<"\n";
+    cout<<"|\"^\"`    |"<<"\n";
+    cout<<"||_/=\\\\__|"<<"\n";
+
+    return 0;
+}  
