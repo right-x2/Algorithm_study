5617a919d2837f999065b261e01068bcb0d5a52e
right-x2 <kjw970103@gmail.com>
2020-01-22 00:44:25 +0900

    dp

--- 11053.cpp
+++ 11053.cpp
@@ -1,66 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 
 using namespace std;
-int arr[1001];
-int ck[1001];
-int mx = 0;
-int dp(int i, int n)
-{
-    int max = 0;
-    int min = arr[i];
-    int cnt;
-    if(ck[i]==0) cnt = 1;
-    else cnt = ck[i];
-    if(ck[i]>mx) mx = ck[i];
-    for (int j = i+1; j < n; ++j)
-    {
-        if(min<arr[j])
-        {
-            if(ck[j]>cnt+1) return 0;
-            else
-            {
-                min = arr[j];
-                cnt = cnt + 1;
-                ck[j] = cnt;
-                if(mx<cnt) mx = cnt;
-            }
-        }
-        else
-        {
-            for (int k = j-2; k >=0; --k)
-            {
-                if(arr[j]>arr[k])
-                {
-                    if(max<ck[k]) 
-                    {
-                       max = ck[k];
-                    }
 
-                }
-            }
-            ck[j] = max + 1;
-            max = 0;
-        }
-    }
-    return 0;
-}
+vector<int> v;
+vector<int> tmp;
+
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
-    int n;
+    int n,a;
     cin>>n;
     for (int i = 0; i < n; ++i)
     {
-        cin>>arr[i];
+        cin>>a;
+        v.push_back(a);
     }
-    ck[0] = 1;
-    for (int i = 0; i < n; ++i)
+    tmp.push_back(v[0]);
+    for (int i = 1; i < n; ++i)
     {
-        dp(i,n);
+        a = v[i];
+        int tsze = tmp.size()-1;
+        if(tmp[tsze]<a) tmp.push_back(a);
+        else if(tmp[tsze]>=a)
+        {
+            if(tsze==0) tmp[0]=a;
+            else
+            {
+                for (int j = tmp.size()-2; j >=0 ; j--)
+                {
+                    if(a>tmp[j])
+                    {
+                        tmp[j+1] = a;
+                        break;
+                    }
+                }
+                if(a<tmp[0]) tmp[0] = a;
+            }
+        }
     }
-    
-    cout<<mx<<"\n";
-    return 0;
+    cout<<tmp.size()<<"\n";
 }  
