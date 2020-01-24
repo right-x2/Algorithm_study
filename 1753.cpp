cd8fb18be5abacc3dd5996e26f8915c34c8b15d4
right-x2 <kjw970103@gmail.com>
2020-01-24 23:13:18 +0900

    bfs

--- 1753.cpp
+++ 1753.cpp
@@ -1,62 +1,82 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <stack>
 #include <algorithm>
+#include <cstdlib>
+#include <stack>
+
 
 using namespace std;
-int INF = 30000000;
-int arr[200001] ;
-vector< pair<int, int> > pv[200001];
 
-int bfs(int v, int sum)
+vector< pair<int,int> >v;
+vector< pair<int,int> >tmp;
+int n,m;
+int arr[20001][20001];
+int c[20001];
+stack< pair<int,int> > stk;
+
+int dfs(int s)
 {
-    for (int i = 0; i < pv[v].size(); ++i)
+    int a,b;
+    c[s] = 1;
+    for (int i = 1; i <=n ; ++i)
     {
-        if(sum+pv[v][i].second < arr[pv[v][i].first]) arr[pv[v][i].first] = sum+pv[v][i].second;
-        bfs(pv[v][i].first,sum+pv[v][i].second);
+        if(arr[s][i]!=0)
+        {
+            stk.push(make_pair(i,arr[s][i]));
+        }
+    }
+    while(!stk.empty())
+    {
+        a = stk.top().first;
+        b = stk.top().second;
+        stk.pop();
+        for (int i = 1; i <=n ; ++i)
+        {
+            if(arr[a][i]!=0&&c[i]==0)
+            {
+                if(arr[s][i]==0)
+                {
+                    
+                    stk.push(make_pair(i,b+arr[a][i]));
+                    arr[s][i] = b+arr[a][i];
+                }
+                else if(arr[s][i]>b+arr[a][i])
+                {
+                    
+                    stk.push(make_pair(i,b+arr[a][i]));
+                    arr[s][i] = b+arr[a][i];
+                }
+            }
+        }
     }
     return 0;
 }
-
 
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
-    int u,v,w,n,k,start;
-    vector<int> ans;
-
-    cin>>n>>k;
-    cin>>start;
+    int s,a,b,c;
+    cin>>n>>m;
+    cin>>s;
+    for (int i = 0; i < m; ++i)
+    {
+        cin>>a>>b>>c;
+        if(arr[a][b]==0)
+        {
+            arr[a][b] = c;
+        }
+        else if(arr[a][b]>c)
+        {
+            arr[a][b] = c;
+        }
+    }
+    dfs(s);
     for (int i = 1; i <= n; ++i)
     {
-        arr[i] = INF; 
+        if(s==i) cout<<0<<"\n";
+        else if(arr[s][i]==0) cout<<"INF"<<"\n";
+        else cout<<arr[s][i]<<"\n";
     }
-    arr[start] = 0;
-    for (int i = 0; i < k; ++i)
-    {
-        cin>>u>>v>>w;
-        pv[u].push_back(make_pair(v,w));
-    }
-
-    for (int i = 0; i < pv[start].size(); ++i)
-    {
-        arr[pv[start][i].first] = pv[start][i].second;
-        bfs(pv[start][i].first,pv[start][i].second);
-    }
-
-    for (int i = 1; i <= n; ++i)
-    {
-        ans.push_back(arr[i]);
-    }
-    sort(ans.begin(),ans.end());
-    for (int i = 0; i < ans.size(); ++i)
-    {
-        if(ans[i]>=3000000) cout<<"INF"<<"\n";
-        else cout<<ans[i]<<"\n";
-    }
-
-    return 0;
-}
+}  
