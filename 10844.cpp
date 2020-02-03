ea23d17aeb4f10554f83080165802dd242ffde89
right-x2 <kjw970103@gmail.com>
2020-02-03 23:48:56 +0900

    dp

--- 10844.cpp
+++ 10844.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
- 
 using namespace std;
 
-int dp(int n, long long ans)
-{
-    long long mul = 2;
-    int cnt = 1;
-    long long to = 3;
-    while(cnt<n)
-    {
-        if(cnt<=3) to = cnt;
-        else to = (to *2)%1000000000; 
-        ans = ((ans*2) -to)%1000000000;
-        cnt++;
-        cout<<ans<<" :ans\n";
-        cout<<to<<" :to\n";
-    }
-    return ans;
-}
+int arr[101][11];
 
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
-    int n;
-    long long ans = 9;
-    cin>>n;
-    cout<<dp(n,ans)<<"\n";
-    return 0;
+    int a;
+    long long sum = 0;
+    for (int i = 1; i <= 9; ++i)
+    {
+        arr[0][i] = 1;
+    }
+    cin>>a;
+    for (int i = 1; i <a; i++)
+    {
+        for (int j = 0; j <= 9; ++j)
+        {
+            if(j==0)
+            {
+                arr[i][j] = arr[i-1][1]%1000000000;
+            }
+            else if(j==9)
+            {
+                arr[i][j] = arr[i-1][8]%1000000000;
+            }
+            else
+            {
+                arr[i][j] = arr[i-1][j-1]%1000000000 + arr[i-1][j+1]%1000000000;
+            }
+        }
+    }
+    for (int i = 0; i <= 9; ++i)
+    {
+        sum = sum + arr[a-1][i];
+    }
+    sum = sum%1000000000;
+    cout<<sum<<"\n";
 }  
