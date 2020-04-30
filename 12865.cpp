2c5ff891563b7ffadddc76944b1e4cbb7bb0ed8f
right-x2 <kjw970103@gmail.com>
2020-04-30 13:28:56 +0900

    napsack

--- 12865.cpp
+++ 12865.cpp
@@ -1,105 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cstdlib>
-
-
 using namespace std;
 
-vector< pair<int,int> >v;
-vector< pair<int,int> >tmp;
-
-int main(int argc, char** argv)
+int arr[101][100001];
+int n,m;
+vector< pair<int,int> >pv;
+int main(int argc, char const *argv[])
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
-    int n,k,a,b,sum = 0,answer = 0;
-    cin>>n>>k;
+    int a,b;
+    cin>>n>>m;
     for (int i = 0; i < n; ++i)
     {
         cin>>a>>b;
-        v.push_back(make_pair(a,b));
+        pv.push_back(make_pair(a,b));
     }
-    sort(v.begin(),v.end());
-    for (int i = 0; i < v.size(); ++i)
+    sort(pv.begin(),pv.end());
+    for (int i = 1; i <=pv.size(); ++i)
     {
-        cout<<v[i].first<<" "<<v[i].second<<"\n";
-    }
-    if(v[0].first<=k)
-    {
-        tmp.push_back(make_pair(v[0].first,v[0].second));
-        sum = v[0].first;
-        for (int i = 1; i < n; ++i)
+        for (int j = 1; j <= m; ++j)
         {
-            int sz = tmp.size()-1;
-            int rsum = 0;
-            int point = 0;
-            int cnt = 0;
-            if(sum+v[i].first<=k)
-            {
-                if(v[i].first==tmp[sz].first)
-                {
-                    if(v[i].second>tmp[sz].second)
-                    {
-                        tmp.pop_back();
-                        tmp.push_back(make_pair(v[i].first,v[i].second));
-                        sum = sum - tmp[sz].second + v[i].second;
-                    }
-                }
-                else
-                {
-                    tmp.push_back(make_pair(v[i].first,v[i].second));
-                    sum = sum + v[i].first;
-                }
-            }
+            if(j<pv[i-1].first)arr[i][j] = arr[i-1][j];
             else
             {
-                
-                while(sz>=0)
-                {
-                    rsum = rsum + tmp[sz].first;
-                    point = point + tmp[sz].second;
-                    cnt++;
-                    cout<<v[i].first<<" dd";
-                    cout<<tmp[sz].first<<" dd\n";
-                    if(rsum>=v[i].first)
-                    {
-                        
-                        if(point<=v[i].second)
-                        {
-
-                            for (int j = 0; j < cnt; ++j)
-                            {
-                                tmp.pop_back();
-                            }
-                            tmp.push_back(make_pair(v[i].first,v[i].second));
-                            
-                        }
-                        break;
-                    }
-                    else if(sum-rsum+v[i].first<=k)
-                    {
-                        if(v[i].second>point)
-                        {
-                            for (int j = 0; j < cnt; ++j)
-                            {
-                                tmp.pop_back();
-                            }
-                            tmp.push_back(make_pair(v[i].first,v[i].second));
-                            break;                            
-                        }
-                    }
-                    sz--;
-                }
+                arr[i][j] = max(pv[i-1].second+arr[i-1][j-pv[i-1].first],arr[i-1][j]);
             }
         }
-        for (int i = 0; i < tmp.size(); ++i)
-        {
-            cout<<tmp[i].second<<"\n";
-            answer = answer + tmp[i].second;
-        }
-        cout<<answer<<"\n";
     }
-    else cout<<0<<"\n";
-}  
+    cout<<arr[n][m]<<"\n";
+    return 0;
+}
