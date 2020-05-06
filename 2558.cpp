8c8a5a04ba7a74b59bd0ce053a0386fa46279ec0
right-x2 <kjw970103@gmail.com>
2020-05-06 23:02:55 +0900

    plus

---
+++ 2558.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int n,m;
+    cin>>n;
+    cin>>m;
+    cout<<n+m<<"\n";
+    return 0;
+}  
