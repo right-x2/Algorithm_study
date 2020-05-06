b1c3a62b2a5702c843d880535e56e4790b3bcc9b
right-x2 <kjw970103@gmail.com>
2020-05-06 22:36:01 +0900

    bfs

---
+++ 15663.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+
+vector<int>v;
+int n,m;
+int arr[100001];
+int solve(int idx,vector<int> ans)
+{
+    if(ans.size()==m)
+    {
+        for (int i = 0; i < ans.size(); ++i)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<"\n"<<"";
+        return 0;
+    }
+    int temp=-1;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if(i!=idx&&v[i]!=temp&&arr[i]==0)
+        {
+            ans.push_back(v[i]);
+            arr[i] = 1;
+            solve(i,ans);
+            arr[i] = 0;
+            temp = ans[ans.size()-1];
+            ans.pop_back();
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    vector<int> ans;
+    int num;
+    cin>>n>>m;
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>num;
+        v.push_back(num);
+    }
+    sort(v.begin(),v.end());
+    int temp = -1;
+    for (int i = 0; i < n; ++i)
+    {
+        if(v[i]!=temp)
+        {
+            ans.push_back(v[i]);
+            arr[i] = 1;
+            solve(i,ans);
+            arr[i] = 0;
+            temp = ans[ans.size()-1];
+            ans.pop_back();
+        }
+    }
+    return 0;
+}  
