59a4d02a7e043de920c33118753f4ec14e5416dc
right-x2 <kjw970103@gmail.com>
2020-06-25 09:17:57 +0900

    simulation

--- 13460.cpp
+++ 13460.cpp
@@ -1,304 +1,370 @@
 #include <iostream>
-
+#include <vector>
+#include <queue>
 using namespace std;
-char cop[11][11];
+
 char arr[11][11];
-int chk[11][11];
-int cb[11];
-int mx = 12,a,b,ry,rx,by,bx,n,m,ary,arx,aby,abx;
-
-int move(int j,int cnt)
+char temp[11][11];
+int ans = 0;
+int bx,by,rx,ry;
+int n,m;
+int run(int idx,int cnt)
 {
-    int a = ry,b = rx ,c = by,d = bx;
-
-    if(cnt>10) return 0; 
-    if(j==0)
+   // cout<<cnt<<"\n";
+    arr[ry][rx] = '.';
+    arr[by][bx] = '.';
+    int flag = 0;
+    if(idx==0)
     {
-            if(arr[ry][rx+1]=='#') return 1;
-            if(rx>bx)
-            {
-                while(arr[ry][rx+1]=='.')
-                {
-                    rx++;
-                }
-                while(arr[by][bx+1]=='.')
-                {
-                    bx++;
-                } 
-                if(arr[ry][rx+1]=='O'&&arr[by][bx+2]=='O') return 0;              
-            }
-            else
-            {
-                while(arr[by][bx+1]=='.')
-                {
-                    bx++;
-                }  
-                while(arr[ry][rx+1]=='.')
-                {
-                    rx++;
-                }
-                if(arr[by][bx+1]=='O'&&arr[ry][rx+2]=='O') return 0;              
-            }
+        if(ry>by)
+        {
 
-            if(arr[ry][rx+1]=='O')
+            for (int i = ry; i <n ; ++i)
             {
-                if(mx>cnt) mx = cnt;
-                return 0;
-            }
-            else if(arr[by][bx+1]=='O')
-            {
-                return 0;
-            }
-            else
-            {
-                if(chk[ry][rx]==1) return 0;
-                else 
+                if(arr[i][rx]=='#')
                 {
-                    chk[ry][rx] = 1;
-                    arr[a][b] ='.';
-                    arr[c][d] ='.';
-                    arr[ry][rx]='R';
-                    arr[by][bx]='B';
-                    return 1;
+                    arr[i-1][rx] = 'R';
+                    break;
                 }
-            }  
-    }
-    if(j==1)
-    {
-        if(arr[ry][rx-1]=='#') return 1;
-            if(rx<bx)
-            {
-                while(arr[ry][rx-1]=='.')
+                else if(arr[i][rx]=='O')
                 {
-                    rx--;
+                    flag = 1;
+                    break;
+                    
                 }
-                while(arr[by][bx-1]=='.')
-                {
-                    bx--;
-                } 
-                if(arr[ry][rx-1]=='O'&&arr[by][bx-2]=='O') return 0;              
             }
-            else
+            for (int i = by; i <n ; ++i)
             {
-                while(arr[by][bx-1]=='.')
+                if(arr[i][bx]=='#'||arr[i][bx]=='R')
                 {
-                    bx--;
-                }  
-                while(arr[ry][rx-1]=='.')
-                {
-                    rx--;
-                }
-                if(arr[by][bx-1]=='O'&&arr[ry][rx-2]=='O') return 0;              
-            }
-
-            if(arr[ry][rx-1]=='O')
-            {
-                if(mx>cnt) mx = cnt;
-                return 0;
-            }
-            else if(arr[by][bx-1]=='O')
-            {
-                return 0;
-            }
-            else
-            {
-                if(chk[ry][rx]==1) return 0;
-                else 
-                {
-                    chk[ry][rx] = 1;
-                    arr[a][b] ='.';
-                    arr[c][d] ='.';
-                    arr[ry][rx]='R';
-                    arr[by][bx]='B';
-                    return 1;
-                }
-            }  
-    }
-    if(j==2)
-    {
-        if(arr[ry-1][rx]=='#') return 1;
-            if(ry<by)
-            {
-                while(arr[ry-1][rx]=='.')
-                {
-                    ry--;
-                }
-                while(arr[by-1][bx]=='.')
-                {
-                    by--;
-                } 
-                if(arr[ry-1][rx]=='O'&&arr[by-2][bx]=='O') return 0;              
-            }
-            else
-            {
-                while(arr[by-1][bx]=='.')
-                {
-                    by--;
-                }  
-                while(arr[ry-1][rx]=='.')
-                {
-                    rx--;
-                }
-                if(arr[by-1][bx]=='O'&&arr[ry-2][rx]=='O') return 0;              
-            }
-
-            if(arr[ry-1][rx]=='O')
-            {
-                if(mx>cnt) mx = cnt;
-                return 0;
-            }
-            else if(arr[by-1][bx]=='O')
-            {
-                return 0;
-            }
-            else
-            {
-                if(chk[ry][rx]==1) return 0;
-                else 
-                {
-                    chk[ry][rx] = 1;
-                    arr[a][b] ='.';
-                    arr[c][d] ='.';
-                    arr[ry][rx]='R';
-                    arr[by][bx]='B';
-                    return 1;
-                }
-            }  
-    }
-    if(j==3)
-    {
-        if(arr[ry+1][rx]=='#') return 1;
-            if(rx>bx)
-            {
-                while(arr[ry+1][rx]=='.')
-                {
-                    ry++;
-                }
-                while(arr[by+1][bx]=='.')
-                {
-                    by++;
-                } 
-                if(arr[ry+1][rx]=='O'&&arr[by+2][bx]=='O') return 0;              
-            }
-            else
-            {
-                while(arr[by+1][bx]=='.')
-                {
-                    by++;
-                }  
-                while(arr[ry+1][rx]=='.')
-                {
-                    ry++;
-                }
-                if(arr[by+1][bx]=='O'&&arr[ry+2][rx]=='O') return 0;              
-            }
-
-            if(arr[ry+1][rx]=='O')
-            {
-                if(mx>cnt) mx = cnt;
-                return 0;
-            }
-            else if(arr[by+1][bx]=='O')
-            {
-                return 0;
-            }
-            else
-            {
-                if(chk[ry][rx]==1) return 0;
-                else 
-                {
-                    chk[ry][rx] = 1;
-                    arr[a][b] ='.';
-                    arr[c][d] ='.';
-                    arr[ry][rx]='R';
-                    arr[by][bx]='B';
-                    for (int i = 0; i < n; ++i)
+                    if(flag==1)
                     {
-                        for (int j = 0; j < m; ++j)
-                        {
-                            cout<<arr[i][j]<<" ";
-                        }
-                        cout<<""<"\n";
+                        if(ans==0||ans>cnt) ans = cnt;
+                        return 0;
                     }
+                    arr[i-1][bx] = 'B';
                     return 1;
                 }
-            }  
+                else if(arr[i][bx]=='O')
+                {
+                    return 0;
+                }
+            }
+        }
+        else
+        {
+            for (int i = by; i <n ; ++i)
+            {
+                if(arr[i][bx]=='#')
+                {
+                    arr[i-1][bx] = 'B';
+                    break;
+                }
+                else if(arr[i][bx]=='O')
+                {
+                    return 0;
+                }
+            }
+            for (int i = ry; i <n ; ++i)
+            {
+                if(arr[i][rx]=='#'||arr[i][rx]=='B')
+                {
+                    arr[i-1][rx] = 'R';
+                    return 1;
+                }
+                else if(arr[i][rx]=='O')
+                {
+                    if(ans==0||ans>cnt) ans = cnt;
+                    return 0;
+                }
+            }
+        }            
     }
+    if(idx==1)
+    {
+        
+        if(rx>bx)
+        {
+            for (int i = rx; i <m ; ++i)
+            {
+                if(arr[ry][i]=='#')
+                {
+                    arr[ry][i-1] = 'R';
+                    break;
+                }
+                else if(arr[ry][i]=='O')
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            for (int i = bx; i <m ; ++i)
+            {
+                if(arr[by][i]=='#'||arr[by][i]=='R')
+                {
+                    if(flag==1)
+                    {
+                        if(ans==0||ans>cnt) ans = cnt;
+                        return 0;
+                    }
+                    arr[by][i-1] = 'B';
+                    return 1;
+                }
+                else if(arr[by][i]=='O')
+                {
+                    return 0;
+                }
+            }
+        }
+        else
+        {
+            for (int i = bx; i <m ; ++i)
+            {
+                if(arr[by][i]=='#')
+                {
+                    arr[by][i-1] = 'B';
+                    break;
+                }
+                else if(arr[by][i]=='O')
+                {
+                    return 0;
+                }
+            }
+            for (int i = rx; i <m ; ++i)
+            {
+                if(arr[ry][i]=='#'||arr[ry][i]=='B')
+                {
+                    arr[ry][i-1] = 'R';
+                    return 1;
+                }
+                else if(arr[ry][i]=='O')
+                {
+                    if(ans==0||ans>cnt) ans = cnt;
+                    return 0;
+                }
+            }
+        } 
+    }
+    if(idx==2)
+    {
+        
+        if(ry<by)
+        {
+            for (int i = ry; i >=0 ; --i)
+            {
+                if(arr[i][rx]=='#')
+                {
+                    arr[i+1][rx] = 'R';
+                    break;
+                }
+                else if(arr[i][rx]=='O')
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            for (int i = by; i >=0 ; --i)
+            {
+                if(arr[i][bx]=='#'||arr[i][bx]=='R')
+                {
+                    if(flag==1)
+                    {
+                        if(ans==0||ans>cnt) ans = cnt;
+                        return 0;
+                    }
+                    arr[i+1][bx] = 'B';
+                    return 1;
+                }
+                else if(arr[i][bx]=='O')
+                {
+                    return 0;
+                }
+            }
+        }
+        else
+        {
+            for (int i = by; i >=0 ; --i)
+            {
+                if(arr[i][bx]=='#')
+                {
+                    arr[i+1][bx] = 'B';
+                    break;
+                }
+                else if(arr[i][bx]=='O')
+                {
+                    return 0;
+                }
+            }
+            for (int i = ry; i >=0 ; --i)
+            {
+                if(arr[i][rx]=='#'||arr[i][rx]=='B')
+                {
+                    arr[i+1][rx] = 'R';
+                    return 1;
+                }
+                else if(arr[i][rx]=='O')
+                {
+                    if(ans==0||ans>cnt) ans = cnt;
+                    return 0;
+                }
+            }
+        }  
+    }
+    if(idx==3)
+    {
+        
+        if(rx<bx)
+        {
+            for (int i = rx; i >=0 ; --i)
+            {
+                if(arr[ry][i]=='#')
+                {
+                    arr[ry][i+1] = 'R';
+                    break;
+                }
+                else if(arr[ry][i]=='O')
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            for (int i = bx; i >=0 ; --i)
+            {
+                if(arr[by][i]=='#'||arr[by][i]=='R')
+                {   
+                    if(flag==1)
+                    {
+                        if(ans==0||ans>cnt) ans = cnt;
+                        return 0;
+                    }
+                    arr[by][i+1] = 'B';
+                    return 1;
+                }
+                else if(arr[by][i]=='O')
+                {
+                    return 0;
+                }
+            }
+        }
+        else
+        {
+            for (int i = bx; i >=0 ; --i)
+            {
+                if(arr[by][i]=='#')
+                {
+                    arr[by][i+1] = 'B';
+                    break;
+                }
+                else if(arr[by][i]=='O')
+                {
+                    return 0;
+                }
+            }
+            for (int i = rx; i >=0 ; --i)
+            {
+                if(arr[ry][i]=='#'||arr[ry][i]=='B')
+                {
+                    arr[ry][i+1] = 'R';
+                    return 1;
+                }
+                else if(arr[ry][i]=='O')
+                {
+                    if(ans==0||ans>cnt) ans = cnt;
+                    return 0;
+                }
+            }
+        } 
+    }
+    return 1;
 }
 
-int play(int cnt)
+int solve(int cnt)
 {
-    for (int i = 0; i < 10; ++i)
+
+    int tmp[21][21];
+    if(cnt>10) return 0;
+    for (int j = 0; j < n; ++j)
     {
-        if(!move(cb[i],i+1))
+        for (int k = 0; k < m; ++k)
         {
-            for (int k = 0; k < a; ++k)
+            tmp[j][k] = arr[j][k];
+        }
+    }
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            for (int k = 0; k < m; ++k)
             {
-                for (int j = 0; j < b; ++j)
+                arr[j][k] = tmp[j][k];
+                if(tmp[j][k]=='R')
                 {
-                    arr[k][j] = cop[k][j];
+                    ry = j;
+                    rx = k;
+                }
+                if(tmp[j][k]=='B')
+                {
+                    by = j;
+                    bx = k;
                 }
             }
-            ry = ary;
-            rx = arx;
-            by = aby;
-            bx = abx;
-            return 0;
+            
         }
+        int bol = run(i,cnt);
+        if(bol==1)solve(cnt+1);
     }
     return 0;
 }
-int cbn(int j,int cnt)
-{
-    if(cnt==10) play(cnt);
-    for (int i = 0; i < 4; ++i)
-    {
-        if(i!=j)
-        {
-            cb[cnt] = i;
-            cbn(i,cnt+1);
-            cb[cnt] = 0;
-        }
-    }
-}
-
-
 
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
+    int tmp[21][21];
     cin>>n>>m;
+    
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < m; ++j)
         {
             cin>>arr[i][j];
-            if(arr[i][j]=='R')
+            tmp[i][j] = arr[i][j];
+            if(tmp[i][j]=='R')
             {
                 ry = i;
                 rx = j;
-                ary = i;
-                arx = j;
             }
-            else if(arr[i][j]=='B')
+            if(tmp[i][j]=='B')
             {
                 by = i;
                 bx = j;
-                aby = i;
-                abx = j;
             }
         }
-    }
-    for (int i = 0; i < n; ++i)
+    } 
+    for (int i = 0; i < 4; ++i)
     {
-        for (int j = 0; j < m; ++j)
+        for (int j = 0; j < n; ++j)
         {
-            cop[i][j] = arr[i][j];
+            for (int k = 0; k < m; ++k)
+            {
+                arr[j][k] = tmp[j][k];
+                if(tmp[j][k]=='R')
+                {
+                    ry = j;
+                    rx = k;
+                }
+                if(tmp[j][k]=='B')
+                {
+                    by = j;
+                    bx = k;
+                }
+            }
         }
+        int bol = run(i,1);
+        if(bol==1)solve(2);
     }
-    cbn(-1,0);
-    cout<<mx<<"\n";
+    if(ans==0)cout<<-1<<"\n";
+    else cout<<ans<<"\n";
     return 0;
-}  
+}
