e36e84e01170b2161b54d03d33666f9ee71a8339
right-x2 <kjw970103@gmail.com>
2020-07-02 23:22:20 +0900

    simulation

---
+++ 6416.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    return 0;
+}
