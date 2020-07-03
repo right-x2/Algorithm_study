fe56d5810ed24524203c63acbac515c0b077a919
right-x2 <kjw970103@gmail.com>
2020-07-03 10:44:39 +0900

    brute force

--- 3671.cpp
+++ 3671.cpp
@@ -4,75 +4,83 @@
 #include <algorithm>
 using namespace std;
 
-int arr[1000001];
-int chk[1000001];
+int arr[10000000];
+int chk[10000000];
 int cidx[8];
 string str;
 int n;
 int answer;
-int solve(int num, int cnt)
+
+int conv(string temp)
 {
-    //cout<<num<<"\n"
-    if(cnt==str.length()) return 0;
-    int mul = 10;
-    for (int i = 1; i < cnt; ++i)
+    int mul = 1;
+    int ans = 0;
+    for (int i = temp.length()-1; i >= 0; --i)
     {
+        ans = ans + (temp[i]-'0')*mul;
         mul = mul * 10;
     }
+    return ans;
+}
+int solve(string temp, int cnt)
+{
+    int num = conv(temp);
+    //cout<<num<<"\n";
+    if(chk[num]==0&&arr[num]==0)
+    {
+        chk[num] = 1;
+        answer++;
+    }
+
     for (int i = 0; i < str.length(); ++i)
     {
-        if(cidx[i]==0)
+        if(cnt<str.length()&&cidx[i]==0)
         {
-            int temp = str[i]-'0';
-            cout<<arr[mul*temp+num]<<" "<<str[i]<<" "<<cnt<<"\n";
-            if(arr[mul*temp+num]==0&&chk[mul*temp+num]==0)
-            {
-
-                answer++;
-                chk[mul*temp+num] = 1;
-            }
-            cidx[i] = 0;
-            solve(mul*temp+num,cnt+1);
             cidx[i] = 1;
+            solve(temp+str[i],cnt+1);
+            cidx[i] = 0;
         }
     }
     return 0;
 }
+
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
-    
-    for(int i = 2; i < 1000001; i++)
+    string temp = "";
+    for(int i = 2; i < 10000000; i++)
     {
         if(arr[i]!=0) continue;
-        for (int j = 2; j < 1000001; ++j)
+        for (int j = 2; j < 10000000; ++j)
         {
-            if(i*j>1000000) break;
+            if(i*j>10000000) break;
             if(arr[i*j]==0) arr[i*j] = 1;
         }
     }
     arr[1] = 1;
     arr[0] = 1;
     cin>>n;
-    for (int i = 0; i < n; ++i)
-    {
+    for(int j = 0; j < n; j++)
+    {   
         cin>>str;
-        for (int j = 0; j < str.length(); ++j)
+        for (int i = 0; i < str.length(); ++i)
         {
-            if(chk[str[j]-'0']==0&&arr[str[j]-'0']==0)
-            {
-                cout<<str[j]-'0'<<"\n";
-                answer++;
-                chk[str[j]-'0'] = 1;
-            }
-            cidx[j] = 0;
-            cout<<str[j]-'0'<<"\n";
-            solve(str[j]-'0',1);
-            cidx[j] = 1;
+            cidx[i] = 1;
+            temp = str[i];
+            solve(temp,1);
+            cidx[i] = 0;
         }
         cout<<answer<<"\n";
+        for (int i = 0; i < 10000000; ++i)
+        {
+            chk[i] = 0;
+        }
+        for (int i = 0; i < 8; ++i)
+        {
+            cidx[i] = 0;
+        }
         answer = 0;
     }
     return 0;
