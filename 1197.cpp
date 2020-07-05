0b8394d2c020d932f665c9f03675ab06aa86bb42
right-x2 <kjw970103@gmail.com>
2020-07-05 23:40:24 +0900

    prim algorithm

--- 1197.cpp
+++ 1197.cpp
@@ -4,9 +4,35 @@
 #include <stack>
 #include <algorithm>
 using namespace std;
-
+long long ans = 0;
 int n,m;
+long long arr[10001];
+int chk[10001];
 vector<pair<int,long long> >v[10001];
+
+int bfs()
+{
+    priority_queue<pair<int,int> >q;
+    q.push(make_pair(0,1));
+    while(!q.empty())
+    {
+        int nde = q.top().second;
+        q.pop();
+        for (int i = 0; i < v[nde].size(); ++i)
+        {
+            if(v[nde][i].second<arr[v[nde][i].first])
+            {
+                if(chk[v[nde][i].first]==0)
+                {
+                   q.push(make_pair(v[nde][i].second,v[nde][i].first));
+                   chk[v[nde][i].first] = 1;
+                }
+                arr[v[nde][i].first] = v[nde][i].second;
+            }
+        }
+    }
+    return 0;
+}
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
@@ -15,11 +41,23 @@ int main(int argc, char** argv)
     int a,b;
     long long c;
     cin>>n>>m;
-    for (int i = 0; i < count; ++i)
+    for (int i = 0; i < m; ++i)
     {
         cin>>a>>b>>c;
         v[a].push_back(make_pair(b,c));
         v[b].push_back(make_pair(a,c));
     }
+    for (int i = 1; i <= n; ++i)
+    {
+        arr[i] = 1000001;
+    }
+    arr[1] = 0;
+    chk[1] = 1;
+    bfs();
+    for (int i = 1; i <= n; ++i)
+    {
+        ans = ans + arr[i];
+    }
+    cout<<ans<<"\n";
     return 0;
 }
