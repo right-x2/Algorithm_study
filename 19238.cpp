55692da93e675570e403550eb265ff4106ff6961
unknown <kjw970103@gmail.com>
2020-08-01 11:37:28 +0900

    bfs&&simulation

--- 19238.cpp
+++ 19238.cpp
@@ -2,126 +2,117 @@
 #include <iostream>
 #include <queue>
 #include <cstdlib>
+#include <algorithm>
 using namespace std;
 
-int arr[401][401];
-int tmp[401][401];
+int n,m,k;
+int arr[21][21];
+int tmp[21][21];
+int dy[4] ={1,-1,0,0};
+int dx[4] ={0,0,-1,1};
+int answer;
 
-int earr[401][401];
+int sy,sx;
 
-long long sum;
-int n,m;
-int sx,sy;
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,-1,0,1};
-
-
-
-int bfs()
+struct P
 {
-    for(int i = 1; i <= n; i++)
+    int y;
+    int x;
+};
+
+
+P box[405];
+
+int bfs(){
+    priority_queue<pair<int,pair<int,int> > ,vector<pair<int,pair<int,int> > >,greater<pair<int,pair<int,int> > > >pq;
+
+    queue<pair<int,pair<int,int> > >q;
+    queue<pair<pair<int,int>,pair<int,int> > >dq;
+    q.push(make_pair(0,make_pair(sy,sx)));
+
+    for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= n; ++j)
         {
             arr[i][j] = tmp[i][j];
         }
     }
-    queue<pair<int,pair<int,int> > >q;
-    q.push(make_pair(0,make_pair(sy,sx)));
-    int ans = -1;
-    int ay = -1;
-    int ax = -1;
-    int num = -1;
 
-    if(arr[sy][sx]<1) arr[sy][sx] = 1;
     while(!q.empty())
     {
-        int dis = q.front().first;
+        int cnt = q.front().first;
         int y = q.front().second.first;
         int x = q.front().second.second;
+        
         q.pop();
-        cout<<y<<" "<<x<<" "<<dis<<" "<<arr[y][x]<<"\n";
-        if(ans!=-1&&ans<dis) break;
-        if(dis>sum) return -1;
         if(arr[y][x]>1)
         {
-            if(ans==-1)
-            {
-
-                ans = dis;
-                ay = y;
-                ax = x;
-                num = arr[y][x];
-                if(dis==0) break;
-            }
-            else if(ans==dis)
-            {
-                if(y<ay)
-                {
-                    ans = dis;
-                    ay = y;
-                    ax = x;
-                    num = arr[y][x];
-                }
-                else if(y==ay&&x<ax)
-                {
-                    ans = dis;
-                    ay = y;
-                    ax = x; 
-                    num = arr[y][x];                   
-                }
-            }
+            pq.push(make_pair(cnt,make_pair(y,x)));
             continue;
         }
-        
-        for (int i = 0; i < 4; ++i)
+        for(int i = 0; i < 4;i++)
         {
-            if(y+dy[i]<=n&&y+dy[i]>0&&x+dx[i]<=n&&x+dx[i]>0&&(arr[y+dy[i]][x+dx[i]]!=1))
+            if(y+dy[i]<=n&&y+dy[i]>0&&x+dx[i]<=n&&x+dx[i]>0)
             {
-                q.push(make_pair(dis+1,make_pair(y+dy[i],x+dx[i])));
-                if(arr[y+dy[i]][x+dx[i]]<=1)arr[y+dy[i]][x+dx[i]] = 1;
+                if(arr[y+dy[i]][x+dx[i]]==0)
+                {
+                    q.push(make_pair(cnt+1,make_pair(y+dy[i],x+dx[i])));
+                    arr[y+dy[i]][x+dx[i]] = 1;
+                }
+                else if(arr[y+dy[i]][x+dx[i]]>1)
+                {
+              //      cout<<y+dy[i]<<" "<<x+dx[i]<<"\n";
+                    pq.push(make_pair(cnt+1,make_pair(y+dy[i],x+dx[i])));
+                    arr[y+dy[i]][x+dx[i]] = 1;
+                }
             }
-        } 
+        }
     }
-    while(!q.empty()) q.pop();
-    
-    if(ans==-1) return -1;
-    tmp[ay][ax] = 0;
-    for(int i = 1; i <= n; i++)
+    if(pq.empty()||pq.top().first>k) return -1;
+
+    int num = tmp[pq.top().second.first][pq.top().second.second];
+    k = k - pq.top().first;
+    dq.push(make_pair(make_pair(k,0),make_pair(pq.top().second.first,pq.top().second.second)));
+
+    tmp[pq.top().second.first][pq.top().second.second] = 0;
+
+  //  cout<<pq.top().second.first<<" "<<pq.top().second.second<<" "<<k<<"\n";
+    for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= n; ++j)
         {
             arr[i][j] = tmp[i][j];
         }
     }
-    arr[ay][ax] = 1;
-    q.push(make_pair(0,make_pair(ay,ax)));
-    while(!q.empty())
+ //   cout<<num<<"\n";
+    while(!dq.empty())
     {
-        int dis = q.front().first;
-        int y = q.front().second.first;
-        int x = q.front().second.second;
-        q.pop();
-        cout<<y<<" "<<x<<" "<<dis<<" "<<earr[y][x]<<"\n";
-        if(dis+ans>sum) return -1;
-        if(earr[y][x]==num)
+        int sum = dq.front().first.first;
+        int cnt = dq.front().first.second;
+        int y = dq.front().second.first;
+        int x = dq.front().second.second;
+
+        dq.pop();
+ //       cout<<y<<" "<<x<<" "<<sum<<"\n";
+        if(y==box[num].y&&x==box[num].x)
         {
-            cout<<sum<<" "<<ans<<" "<<dis<<"----------\n";
-            sum = sum-ans-dis+dis*2;
+            k = k + cnt*2 -cnt;
             sy = y;
             sx = x;
             return 0;
         }
-        for (int i = 0; i < 4; ++i)
+
+        if(sum==0) continue;
+
+        for(int i = 0; i < 4;i++)
         {
             if(y+dy[i]<=n&&y+dy[i]>0&&x+dx[i]<=n&&x+dx[i]>0&&arr[y+dy[i]][x+dx[i]]!=1)
             {
-                q.push(make_pair(dis+1,make_pair(y+dy[i],x+dx[i])));
+                dq.push(make_pair(make_pair(sum-1,cnt+1),make_pair(y+dy[i],x+dx[i])));
                 arr[y+dy[i]][x+dx[i]] = 1;
             }
         }
     }
-    
     return -1;
 }
 
@@ -131,7 +122,9 @@ int main(int argc, char** argv)
     cin.tie(NULL); 
     cout.tie(NULL);
     int a,b,c,d;
-    cin>>n>>m>>sum;
+
+    cin>>n>>m>>k;
+
 
     for (int i = 1; i <= n; ++i)
     {
@@ -144,17 +137,19 @@ int main(int argc, char** argv)
 
     cin>>sy>>sx;
 
-    for (int i = 2; i <= m+1; ++i)
+    for (int i = 2; i < m+2; ++i)
     {
         cin>>a>>b>>c>>d;
 
         arr[a][b] = i;
-        earr[c][d] = i;
         tmp[a][b] = i;
+
+        P p;
+        p.y = c;
+        p.x = d;
+        box[i] = p;
     }
 
-    int answer = -1;
-    
     for (int i = 0; i < m; ++i)
     {
         if(bfs()==-1)
@@ -162,13 +157,9 @@ int main(int argc, char** argv)
             answer = -1;
             break;
         }
-        else
-        {
-            answer = sum;
-        }
-        cout<<answer<<" --answer\n";
+        else answer = k;
     }
-    
+
     cout<<answer<<"\n";
     return 0;
 }
