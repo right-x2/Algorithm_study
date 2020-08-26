e46f29a3703c22c1131c737f5e6f4f91e50f54aa
unknown <kjw970103@gmail.com>
2020-08-26 22:54:32 +0900

    tsp

--- 10971.cpp
+++ 10971.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 using namespace std;
 
+int arr[10][10];
 int n;
-long long arr[11][11];
-int chk[11];
-long long mn = -1;
 
-int dfs(int idx, int s, long long sum, int cnt)
+int solve(int start , int pos, int visited)
 {
-    if(cnt==n&&arr[idx][s]!=0)
+    if(visited==(1 << n) -1)
     {
-        sum = sum + arr[idx][s];
-        if(mn==-1||mn>sum) mn = sum;
-        return 0;
+        return arr[pos][start];
     }
+
+    int ret = 876554321;
+
     for (int i = 0; i < n; ++i)
     {
-        if(chk[i]==0&&arr[idx][i]!=0)
+        if(!(visited&(1 << i)) && arr[pos][i])
         {
-            chk[i] = 1;
-            dfs(i,s,sum+arr[idx][i],cnt+1);
-            chk[i] = 0;
+            int tmp = arr[pos][i] + solve(start,i,visited|(1 << i));
+            if(tmp<ret) ret = tmp;
         }
     }
-    return 0;
+
+    return ret;
 }
 
 int main(int argc, char** argv)
@@ -34,19 +32,25 @@ int main(int argc, char** argv)
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
+
     cin>>n;
+
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < n; ++j)
         {
             cin>>arr[i][j];
+            if(arr[i][j]==0) arr[i][j] = 876554321;
         }
     }
+
+    int ans = 876554321;
     for (int i = 0; i < n; ++i)
     {
-        chk[i] = 1;
-        dfs(i,i,0,1);
-        chk[i] = 0;
+        int temp = solve(i,i,1 << i);
+        if(ans>temp) ans = temp;
     }
-    cout<<mn<<"\n";
-}  
+
+    cout<<ans<<"\n";
+    return 0;
+}
