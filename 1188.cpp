07d755dd776a9d2411e389df0a502b400f164528
unknown <kjw970103@gmail.com>
2020-12-25 00:33:29 +0900

    gcd

--- 1188.cpp
+++ 1188.cpp
@@ -4,11 +4,27 @@
 
 using namespace std;
 
+
+int n,m;
+
+
+int solve(int a, int b)
+
+{
+
+    if (a%b == 0)return b;
+    return solve(b, a%b);
+
+}
+
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(false); 
     cin.tie(NULL); 
     cout.tie(NULL);
 
+
+    cin>>n>>m;
+    cout<<m-solve(n,m)<<"\n";
     return 0;
 }
