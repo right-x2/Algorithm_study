564fa6f3dceccf439ed27a67859f76484e50b8e9
unknown <kjw970103@gmail.com>
2020-12-24 23:45:11 +0900

    test

---
+++ 1188.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    return 0;
+}
